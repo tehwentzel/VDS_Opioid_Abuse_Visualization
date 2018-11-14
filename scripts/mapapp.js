79ac943bba5f149d42e3d7816d78deff01ff5840
Andrew Wentzel <tehwentzel@gmail.com>
2018-11-14 17:52:43 -0600

    formatting and tooltip

--- scripts/mapapp.js
+++ scripts/mapapp.js
@@ -25,6 +25,19 @@ var drawMap = function(data,target,color = 'red'){
     var g = svgPatient.append("g").attr("class", "leaflet-zoom-hide");
 
 	console.log("data: ", data);
+
+	// var hoverdiv;
+	// if(target="maps"){
+	var hoverdiv = d3.select("body").append("div")   
+        .attr("class", "tooltippatient")               
+        .style("opacity", 0);
+    // }
+    //    else{
+    //    	var hoverdiv = d3.select("body").append("div")   
+    //     .attr("class", "tooltippharmacy")               
+    //     .style("opacity", 0);
+
+    //    }
    
 //         
 
@@ -37,9 +50,21 @@ var drawMap = function(data,target,color = 'red'){
 		.attr('cy', function(d){ return project(findlatlng(d.x,d.y)).y})
 		.attr('r','4')
 		.attr('stroke', '2px white')
-		.attr('fill',  color)
+		.attr('fill', color)
 		.attr('opacity', 1);
 	dots.call(formatDots);
+
+	function tooltip(d){
+		var stringvalue;
+		if(target=="map"){
+			 stringvalue = "OverFlow Index:" + d.overflow_idx;
+			 return stringvalue;
+		}
+			else{
+				stringvalue = "Patient Count:" + d.patient_count;
+				return stringvalue;
+			}
+	}
 
 	function render(){
 		var bounds = map.getBounds();
@@ -52,7 +77,18 @@ var drawMap = function(data,target,color = 'red'){
 		.style("top", topLeft.y + "px");
 		g.attr("transform", "translate(" + -topLeft.x + "," + -topLeft.y + ")");
 		dots.call(formatDots);
-		dots.attr("transform", "translate(" + -topLeft.x + "," + -topLeft.y + ")");
+		dots.attr("transform", "translate(" + -topLeft.x + "," + -topLeft.y + ")")
+		.on("mouseover", function(d){   
+                      d3.select(this).classed('active', true)
+                      hoverdiv.transition()      
+                .duration(200)      
+                .style("opacity", .9)
+                hoverdiv.html(tooltip(d))
+                .style("left", (d3.event.pageX) + "px")     
+                .style("top", (d3.event.pageY) + "px");
+                //  .style("left","50px")     
+                // .style("top","50px");
+            });
 //           
 	}
 
