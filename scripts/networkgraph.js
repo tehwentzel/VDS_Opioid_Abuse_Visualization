8662d9fdd7525fc246684dcca3c2ab8f51cdd310
Harsh <harshshah6595@gmail.com>
2018-11-14 13:03:22 -0600

    Effects to node graph
    added colors, link size adjust and svg 100% width

--- scripts/networkgraph.js
+++ scripts/networkgraph.js
@@ -1,105 +1,4 @@
-/*var App = App || {};
-class NetworkGraph{
-    
-    constructor(pat_id, NODE_GRAPH){
-		this.NODE_GRAPH = NODE_GRAPH;
-		this.height = 140;
-		this.baseColor = '#2ca25f';
-		this.div = d3.select("#network")
-			.append('div')
-			.attr('class','tooltip')
-			.style('opacity', 0);
-		this.svg = d3.select("#gantt-chart")
-			.append('svg')
-			.attr('width','90%')
-			.attr('height', this.height + 'px');
-		this.width = this.svg.node().clientWidth;
-        this.struct=this.createStruct(pat_id,NODE_GRAPH);
-        console.log(this.struct);
-        finalGraph(this.struct);
-    }
-    
-    createStruct(selected_id,NODE_GRAPH)
-    {
-        var struct={};
-        struct.nodes=[];
-        struct.links=[];
-        struct.nodes.push({"id":selected_id,"group":"patient"});
-        NODE_GRAPH.forEach(function(d){
-            
-            if (d.pat_id==selected_id)
-            {
-                let doctor_node = {"id":d.physiciannpi,"group":"doctor"};
-                let pharmacy_node={"id":d.pharmacynpi,"group":"pharmacy"};
-                struct.nodes.push(doctor_node);
-                struct.nodes.push(pharmacy_node);
-                let doctor_link={"source":selected_id,"target":d.physiciannpi}
-                let pharmacy_link={"source":selected_id,"target":d.pharmacynpi}
-                struct.links.push(doctor_link);
-                struct.links.push(pharmacy_link);
-            }
-       
-        })
-        return struct;
-        
-    }
 
-    finalGraph(struct)
-    {
-        this.width = 960,
-            height = 500
-
-this.svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height);
-
-var force = d3.layout.force()
-    .gravity(.05)
-    .distance(100)
-    .charge(-100)
-    .size([width, height]);
-
-d3.json("graphFile.json", function(json) {
-  force
-      .nodes(json.nodes)
-      .links(json.links)
-      .start();
-
-  var link = svg.selectAll(".link")
-      .data(json.links)
-    .enter().append("line")
-      .attr("class", "link")
-    .style("stroke-width", function(d) { return Math.sqrt(d.weight); });
-
-  var node = svg.selectAll(".node")
-      .data(json.nodes)
-    .enter().append("g")
-      .attr("class", "node")
-      .call(force.drag);
-
-  node.append("circle")
-      .attr("r","5");
-
-  node.append("text")
-      .attr("dx", 12)
-      .attr("dy", ".35em")
-      .text(function(d) { return d.name });
-
-  force.on("tick", function() {
-    link.attr("x1", function(d) { return d.source.x; })
-        .attr("y1", function(d) { return d.source.y; })
-        .attr("x2", function(d) { return d.target.x; })
-        .attr("y2", function(d) { return d.target.y; });
-
-    node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
-  });
-});
-    }
-
-
-    
-
-}*/
 function createStruct(selected_id,NODE_GRAPH)
     {
         var struct={};
@@ -128,8 +27,9 @@ function createStruct(selected_id,NODE_GRAPH)
 function tryS(struct){
 
 var svg = d3.select("#network").append("svg")
-.attr("width",900)
+.attr("width",1500)
 .attr("height",600)
+.classed("svg-container",true)
 width = +svg.attr("width"),
 height = +svg.attr("height");
 
@@ -137,9 +37,9 @@ var simulation = d3.forceSimulation()
 .force("link", d3.forceLink().id(function(d) { return d.id; }))
 //.force("charge", d3.forceManyBody().strength(-200))
     .force('charge', d3.forceManyBody()
-  .strength(-200)
+  .strength(-900)
   .theta(0.8)
-  .distanceMax(150)
+  .distanceMax(10050)
 )
 // 		.force('collide', d3.forceCollide()
 //       .radius(d => 40)
@@ -187,11 +87,12 @@ var link = svg.append("g")
 
 var node = svg.append("g")
         .attr("class", "nodes")
-.selectAll("circle")
+        .selectAll("circle")
         .data(graph.nodes)
-.enter().append("circle")
-      .attr("r", 2)
-      .call(d3.drag()
+        .enter().append("circle")
+        .attr("r", 2)
+        .on("click",clicked)
+        .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
           .on("end", dragended));
@@ -219,8 +120,15 @@ link
     .attr("y2", function(d) { return d.target.y; });
 
 node
-     .attr("r", 16)
-     .style("fill", "#efefef")
+     .attr("r", 10)
+     .style("fill", function(d){
+         if(d.group=="patient") 
+            return "green";
+        if(d.group=="pharmacy")
+            return "blue";
+        else{
+            return "red";
+        }})
      .style("stroke", "#424242")
      .style("stroke-width", "1px")
      .attr("cx", function (d) { return d.x+5; })
@@ -251,6 +159,10 @@ d.fx = d3.event.x
 d.fy = d3.event.y
 if (!d3.event.active) simulation.alphaTarget(0);
 //simulation.unfix(d);
+}
+function clicked(d){
+    console.log("heieie")
+    console.log("done")
 }
 
 run(graph)
